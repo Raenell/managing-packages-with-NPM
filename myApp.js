bb524e96c131e9ba894307a96a22b33d00c10a59
Raenell <raenelljavier@gmail.com>
2021-04-10 00:05:12 -0400

    Add a json route with dummy data

--- myApp.js
+++ myApp.js
@@ -5,15 +5,21 @@ var bGround = require('fcc-express-bground');
 bGround.log("Hello World");
 console.log("Hello World");
 
-
-app.use(express.static(__dirname + "/public/"));
-app.use('/public', express.static(__dirname + "/public"));
-
-
+//Serve an HTML file
 app.get("/", function(req, res){
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//Serve static assets
+app.use(express.static(__dirname + "/public/"));
+app.use('/public', express.static(__dirname + "/public"));
+
+//Serve json with a specific route
+app.get("/json", function(req, res) {
+  res.json(
+    { "message": "Hello json" }
+  );
+});
 
 
 
