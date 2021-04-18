faeb569d93eea6af4e909154cbed6c78c6ae507a
Raenell <raenelljavier@gmail.com>
2021-04-18 14:21:54 -0400

    Add a middleware logger

--- myApp.js
+++ myApp.js
@@ -3,6 +3,13 @@ var app = express();
 var bGround = require('fcc-express-bground');
 require('dotenv').config();
 
+app.use(function(req, res, next){
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+})
+
+
+
 
 bGround.log("Hello World");
 console.log("Hello World");
@@ -30,6 +37,9 @@ app.get("/json", function(req, res) {
   if (process.env.MESSAGE_STYLE === "uppercase") {
     jsonResponse.message = jsonResponse.message.toUpperCase()
   }
+
+
+
 
   res.json(jsonResponse);
 });
